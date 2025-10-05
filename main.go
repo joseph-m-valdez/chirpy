aaf4b768829c1bbf7fe0c506c3c1411ca9e0c013
Joe <joseph.m.valdez1@gmail.com>
2025-10-05 15:34:15 -0700

    custom handlers

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"net/http"
 )
 
+func healthHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", healthHandler)
 
-	server := http.Server {
+	server := &http.Server {
 		Addr: 		":" + port,
 		Handler:	mux,
 	}
 	
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
